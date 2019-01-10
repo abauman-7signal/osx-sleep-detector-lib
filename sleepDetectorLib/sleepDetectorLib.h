ec08c2e7ec20491d59e509f10d31af23de03ed59
Aaron Bauman <aaron.bauman@7signal.com>
2019-01-10 14:25:44 -0500

    Initial Commit

---
+++ sleepDetectorLib/sleepDetectorLib.h
@@ -0,0 +1,13 @@
+//
+//  sleepDetectorLib.h
+//  sleepDetectorLib
+//
+//  Created by Aaron Bauman on 1/10/19.
+//  Copyright © 2019 Aaron Bauman. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sleepDetectorLib : NSObject
+
+@end
---
+++ sleepDetectorLib/sleepDetectorLib.m
@@ -0,0 +1,13 @@
+//
+//  sleepDetectorLib.m
+//  sleepDetectorLib
+//
+//  Created by Aaron Bauman on 1/10/19.
+//  Copyright © 2019 Aaron Bauman. All rights reserved.
+//
+
+#import "sleepDetectorLib.h"
+
+@implementation sleepDetectorLib
+
+@end
