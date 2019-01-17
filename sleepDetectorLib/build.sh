98fc4a8bf60f61f3c09fbbbc4891d4f48a56e4e5
Aaron Bauman <aaron.bauman@7signal.com>
2019-01-17 11:32:39 -0500

    adds build script

---
+++ sleepDetectorLib/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+function buildObjectiveCLibrary() {
+  clang -framework Foundation -dynamiclib sleepDetectorLib.m -exported_symbols_list export_list -o libsleepDetectorLib.dylib
+  # clang -framework Foundation -dynamiclib sleepDetectorLib.m -o libsleepDetectorLib.dylib
+}
+
+function buildObjectiveCTestClient() {
+  clang -framework Foundation Client.m -o client
+}
+
+function runObjectiveCTestClient() {
+  ./client
+}
+
+function buildCTestClient() {
+  gcc C-Client.c
+}
+
+function runCTestClient() {
+  ./a.out
+}
+
+function buildAndRunForCTestClient() {
+  buildObjectiveCLibrary
+  buildCTestClient
+  runCTestClient
+}
+
+function buildAndRunForObjectiveCTestClient() {
+  buildObjectiveCLibrary
+  buildObjectiveCTestClient
+  runObjectiveCTestClient
+}
+
+# buildAndRunForCTestClient
+buildAndRunForObjectiveCTestClient
