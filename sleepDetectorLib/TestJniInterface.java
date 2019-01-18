d9f95cb882e17bb48ee1171696ed6ccd9eaf73ac
Aaron Bauman <aaron.bauman@7signal.com>
2019-01-18 18:24:54 -0500

    sticks in infinite loop so that notifications can be caught and processed

--- sleepDetectorLib/TestJniInterface.java
+++ sleepDetectorLib/TestJniInterface.java
@@ -8,5 +8,14 @@ public class TestJniInterface {
   public static void main(String[] argv) {
     System.out.printf("Invoking JNI method...\n");
     new JniInterface().log("Hello from " + (new TestJniInterface()).getClass().getSimpleName());
+
+    System.out.printf("Type Ctl+C to exit!\n");
+    while(true) {
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException ex) {
+
+      }
+    }
   }
 }
